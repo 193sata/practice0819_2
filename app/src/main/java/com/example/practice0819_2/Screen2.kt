287668551c5ae7884f97ee389465e79a6ce82657
sataikumi <sata19300@gmail.com>
2024-08-19 15:27:39 +0900

    ボタンを追加

---
+++ app/src/main/java/com/example/practice0819_2/Screen2.kt
@@ -0,0 +1,20 @@
+package com.example.practice0819_2
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+
+class Screen2 {
+    @Composable
+    fun Content() {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            BasicText(text = "これは画面2です")
+        }
+    }
+}
